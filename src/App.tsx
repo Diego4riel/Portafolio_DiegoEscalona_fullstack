0faff17c7e109e9408e196ff57fd8bb85f6fe13d
Diego Escalona <diegoescalona299@gmail.com>
2026-06-06 18:13:00 -0400

    feat: configurar TailwindCSS v4 y shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind funcionando
+      </h1>
+      <Button>Botón de prueba</Button>
+    </div>
+  )
+}
+
+export default App
